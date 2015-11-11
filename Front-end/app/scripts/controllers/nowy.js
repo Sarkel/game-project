ce5b732d072f5e1628887495b867f0764fdc0732
KertulPL <tropciosfera@wp.pl>
2015-11-11 20:25:33 +0100

    a

---
+++ Front-end/app/scripts/controllers/nowy.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the frontEndApp
+ */
+angular.module('frontEndApp')
+	.controller('MojCtrl', ['$scope', function MojCtrl($scope){
+
+	
+	}]);
+	
